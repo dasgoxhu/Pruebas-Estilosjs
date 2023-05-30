18bec58fe1fb7a38fcd23415b47b236ea19d7891
dasgoxhu <78864920+dasgoxhu@users.noreply.github.com>
2023-05-30 17:31:50 -0500

    Add files via upload

---
+++ javaS.js
@@ -0,0 +1,18 @@
+function cambio(){
+	document.getElementById("B1").innerHTML = "<strong>Hugo</strong>";
+}
+function cambio2(){
+	document.getElementById("B1").innerHTML = "Hugo";
+}
+function Acambio(){
+	document.getElementById("B2").innerHTML = "<strong>Paco</strong>";
+}
+function Acambio2(){
+	document.getElementById("B2").innerHTML = "Paco";
+}
+function Bcambio(){
+	document.getElementById("B3").innerHTML = "<strong>Luis</strong>";
+}
+function Bcambio2(){
+	document.getElementById("B3").innerHTML = "Luis";
+}
